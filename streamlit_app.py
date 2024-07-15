a2ba4570e4719b5e87073e7574a070d0feb3d4c5
nguyenhoa-uit <hoaforever2002@gmail.com>
2024-07-15 10:16:47 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 import os
 import textwrap
 
-# import chromadb
+import chromadb
 import langchain
 import openai
 from langchain.chains import RetrievalQA
@@ -21,6 +21,23 @@ from pdf2image import convert_from_path
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
@@ -32,13 +49,76 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+
+
+
+
+
+
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
+    os.environ["OPENAI_API_KEY"]
 
     # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
+
+    model = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    images = convert_from_path("quychetext.pdf", dpi=88)
+    # len(images)
+    # images[0]
+
+    # Use UnstructuredPDFLoader to load PDFs from the Internets
+    pdf_loader = UnstructuredPDFLoader("quychetext.pdf")
+    pdf_pages = pdf_loader.load_and_split()
+
+    # Text Splitters
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
+    texts = text_splitter.split_documents(pdf_pages)
+    # len(texts)
+
+    MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
+    hf_embeddings = HuggingFaceEmbeddings(model_name=MODEL_NAME)
+
+    db = Chroma.from_documents(texts, hf_embeddings, persist_directory="db")
+
+    custom_prompt_template = """Sử dụng các thông tin sau đây để trả lời câu hỏi của người dùng.
+    Nếu bạn không biết câu trả lời, chỉ cần nói rằng bạn không biết, đừng cố bịa ra câu trả lời.
+    Tất cả câu trả lời của bạn đều phải trả lời bằng tiếng việt
+
+    Context: {context}
+    Question: {question}
+
+    """
+
+
+    from langchain import PromptTemplate
+    def set_custom_prompt():
+        """
+        Prompt template for QA retrieval for each vectorstore
+        """
+        prompt = PromptTemplate(template=custom_prompt_template,
+                                input_variables=['context', 'question'])
+        return prompt
+
+    prompt = set_custom_prompt()
+    chain = RetrievalQA.from_chain_type(
+        llm=model,
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 2}),
+        chain_type_kwargs={'prompt': prompt}
+    )
+
+    def print_response(response: str):
+        print("\n".join(textwrap.wrap(response, width=100)))
+
+    query = "Phạm vi áp dụng có nằm trong mục đích hay không?"
+    response = chain.run(query)
+    # print_response(response)
+
+
+
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -60,17 +140,17 @@ else:
             st.markdown(prompt)
 
         # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+        # stream = client.chat.completions.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": m["role"], "content": m["content"]}
+        #         for m in st.session_state.messages
+        #     ],
+        #     stream=True,
+        # )
 
         # Stream the response to the chat using `st.write_stream`, then store it in 
         # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
+        # with st.chat_message("assistant"):
+        #     response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
